a87b58251f723b216824e04c28d1acfe5560d5eb
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2024-06-05 08:56:18 +0200

    refactor: Remove unused BlurredBackground component
    This commit removes the unused BlurredBackground component from the FactLayout in order to improve code cleanliness and reduce unnecessary dependencies.

--- app/dashboard/fact/layout.tsx
+++ app/dashboard/fact/layout.tsx
@@ -1,5 +1,4 @@
 import { LogoIcon } from '@/components/svg';
-import { BlurredBackground } from '@/components/theme';
 
 export default function FactLayout({
 	children,
@@ -9,7 +8,6 @@ export default function FactLayout({
 	return (
 		<main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
 			<div className='flex flex-1 items-center justify-center rounded-lg shadow-sm'>
-				<BlurredBackground />
 				<div className='relative flex flex-col items-center'>
 					<LogoIcon className='w-32' />
 
